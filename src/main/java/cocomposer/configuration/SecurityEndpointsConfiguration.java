3084745d0d7e3f8dbacfabc3034b320b33a1f1b3
Rémi Venant <remi.venant@univ-lemans.fr>
2023-11-28 16:01:04 +0100

    set cors enhancement as a profile.

--- src/main/java/cocomposer/configuration/SecurityEndpointsConfiguration.java
+++ src/main/java/cocomposer/configuration/SecurityEndpointsConfiguration.java
@@ -21,9 +21,15 @@ package cocomposer.configuration;
 import cocomposer.security.authentification.RestAuthenticationFilter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
@@ -38,6 +44,7 @@ import org.springframework.security.web.access.AccessDeniedHandlerImpl;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -52,6 +59,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 //@EnableWebSocketSecurity
 public class SecurityEndpointsConfiguration {
 
+    private static final Log LOG = LogFactory.getLog(SecurityEndpointsConfiguration.class);
+
+    @Value("${server.servlet.session.cookie.name:JSESSIONID}")
+    private String sessionCookieName;
+    
     @Bean
     public DaoAuthenticationProvider mongoLocalAuthenticatitionProvider(UserDetailsService userDetailsService, PasswordEncoder encoder) {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -64,7 +76,7 @@ public class SecurityEndpointsConfiguration {
     public AuthenticationManager authenticationManager(List<AuthenticationProvider> authenticationProviders) {
         return new ProviderManager(authenticationProviders);
     }
-    
+
     @Bean
     public HttpSessionSecurityContextRepository securityContextRepository() {
         return new HttpSessionSecurityContextRepository();
@@ -84,14 +96,22 @@ public class SecurityEndpointsConfiguration {
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    @Profile("cors")
+    CorsConfiguration corsConfiguration() {
         final CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "http://127.0.0.1:*", "moz-extension://*"));
         configuration.setAllowedMethods(Arrays.asList("OPTIONS", "HEAD", "GET", "POST", "PUT", "PATCH", "DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("content-type", "Accept", "Accept-Language", "Authorization", "X-Requested-With"));
+        configuration.setAllowedHeaders(Arrays.asList("content-type", "Accept", "Accept-Language", "Authorization", "X-Requested-With", "x-xsrf-token"));
         configuration.setAllowCredentials(Boolean.TRUE);
+        return configuration;
+    }
+
+    @Bean
+    @Profile("cors")
+    CorsConfigurationSource corsConfigurationSource(CorsConfiguration corsConfiguration) {
+        LOG.warn("CONFIGURE HTTP SECURITY WITH CORS");
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/**", corsConfiguration);
         return source;
     }
 
@@ -99,12 +119,21 @@ public class SecurityEndpointsConfiguration {
     @Bean
     public SecurityFilterChain MultiSecFilterChain(HttpSecurity http,
             RestAuthenticationFilter restAuthenticationFilter,
-            CorsConfigurationSource corsConfigurationSource) throws Exception {
+            Optional<CorsConfigurationSource> corsConfigurationSource) throws Exception {
         return http
                 .exceptionHandling(eh -> eh.accessDeniedHandler(new AccessDeniedHandlerImpl())) // return a simple 403 error if unauthorized
-                .cors(cors -> cors.configurationSource(corsConfigurationSource)) // Enable cors for localhost
+                .cors(cors -> {
+                    if (corsConfigurationSource.isPresent()) {
+                        cors.configurationSource(corsConfigurationSource.get());
+                    }
+                }) // Enable cors for localhost
                 .addFilterAt(restAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // Add the rest authentication filter
-                .logout(logoutCustomizer -> logoutCustomizer.logoutUrl("/api/logout").deleteCookies("JSESSIONID")) // Set logout uri and enforce session cookie deletion
+                .logout(logoutCustomizer -> logoutCustomizer.logoutUrl("/api/logout")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies(this.sessionCookieName)
+                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT))
+                ) // Set logout uri and enforce session cookie deletion
                 .sessionManagement(sm -> {
                     sm.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).maximumSessions(1);
                     sm.sessionFixation(sf -> sf.newSession());
